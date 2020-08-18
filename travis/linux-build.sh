c754882462eed340fa49df78eb8da71dc5028354
Vito Gamberini <vito@gamberini.email>
2020-08-18 06:35:29 -0400

    Maybe fixed

--- travis/linux-build.sh
+++ travis/linux-build.sh
@@ -1,4 +1,7 @@
-echo $PATH
+# Remove the pre-installed cmake from path
+PATH=`echo ${PATH} | awk -v RS=: -v ORS=: '/cmake/ {next} {print}'`
+# Cleanup some weird whitespace issues
+PATH={$PATH%[:space:]*}
 cmake --version
 cmake . -G Ninja
 cmake --build . --target main
